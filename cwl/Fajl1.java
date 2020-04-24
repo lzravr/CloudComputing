11044d3b9d969cf39cec06547a0c60b03a98d87b
Lazar <lzr.avr@gmail.com>
2020-04-24 14:18:37 +0200

    cwl 0.75

---
+++ cwl/Fajl1.java
@@ -0,0 +1,15 @@
+class Klasa3
+{
+    public static void main(String[] args) 
+    {
+        new Klasa5().funkcija1();
+    }
+}
+
+class Klasa5
+{
+    void funkcija1()
+    {
+        System.out.println("Klasa5.funkcija1");
+    }
+}
